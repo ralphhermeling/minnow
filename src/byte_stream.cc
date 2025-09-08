1e7332fb5934b39a08533963be469c49524916c3
Ralph Hermeling <ralphhermeling@hotmail.com>
2025-09-08 15:38:25 +0200

    Implement an in-memory reliable byte stream

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,56 +1,66 @@
 #include "byte_stream.hh"
+#include <algorithm>
+#include <ranges>
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), buffer_() {
+  buffer_.reserve(capacity);
+}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  const auto space_available = capacity_ - buffer_.size();
+  const auto bytes_to_push = std::min(data.size(), space_available);
+  
+  buffer_ += data.substr(0, bytes_to_push);
+  bytes_pushed_ += bytes_to_push;
 }
 
 void Writer::close()
 {
-  // Your code here.
+  is_closed_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return is_closed_; 
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - buffer_.size();
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  return string_view(buffer_);
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  const auto bytes_to_pop = std::min(len, buffer_.size());
+  buffer_.erase(0, bytes_to_pop);
+  bytes_popped_ += bytes_to_pop;
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return is_closed_ && !buffer_.size();
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer_.size(); 
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return bytes_popped_;
 }
 
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -25,6 +25,12 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+
+  std::string buffer_;
+  uint64_t bytes_pushed_ = 0;
+  uint64_t bytes_popped_ = 0;
+
+  bool is_closed_ = false;
 };
 
 class Writer : public ByteStream
